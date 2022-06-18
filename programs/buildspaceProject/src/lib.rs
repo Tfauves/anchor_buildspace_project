a81c375d552ad80e0e88972b9d523892cd992a1c
Tranquility Base <tfauvelle@gmail.com>
2022-06-18 13:04:53 -0400

    program finished and deployed to devnet

--- programs/buildspaceProject/src/lib.rs
+++ programs/buildspaceProject/src/lib.rs
@@ -11,6 +11,21 @@ pub mod buildspace_project {
         base_account.total_gifs = 0;
         Ok(())
     }
+
+    pub fn add_gif(ctx: Context<AddGif>, gif_link: String) -> Result <()> {
+        let base_account = &mut ctx.accounts.base_account;
+        let user = &mut ctx.accounts.user;
+        base_account.total_gifs += 1;
+
+        let item = ItemStruct {
+            gif_link: gif_link.to_string(),
+            user_address: *user.to_account_info().key,
+        };
+
+        base_account.gif_list.push(item);
+        base_account.total_gifs += 1;
+        Ok(())
+    }
 }
 
 #[derive(Accounts)]
@@ -23,7 +38,22 @@ pub struct Initialize<'info> {
     pub system_program: AccountInfo<'info>,
 }
 
+#[derive(Accounts)]
+pub struct AddGif<'info> {
+    #[account(mut)]
+    pub base_account: Account<'info, BaseAccount>,
+    #[account(mut)]
+    pub user: Signer<'info>,
+}
+
+#[derive(Debug, Clone, AnchorSerialize, AnchorDeserialize)]
+pub struct ItemStruct {
+    pub gif_link: String,
+    pub user_address: Pubkey,
+}
+
 #[account]
 pub struct BaseAccount {
     pub total_gifs: u64,
+    pub gif_list: Vec<ItemStruct>,
 }
--- tests/buildspaceProject.ts
+++ tests/buildspaceProject.ts
@@ -1,16 +1,26 @@
 import * as anchor from "@project-serum/anchor";
 import { Program } from "@project-serum/anchor";
 import { BuildspaceProject } from "../target/types/buildspace_project";
+import { PublicKey, SystemProgram, Transaction, Connection, Commitment } from '@solana/web3.js';
 
 describe("buildspaceProject", () => {
   // Configure the client to use the local cluster.
   anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.getProvider();
+
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.BuildspaceProject as Program<BuildspaceProject>;
+  const baseAccount = anchor.web3.Keypair.generate();
 
   it("Is initialized!", async () => {
     // Add your test here.
-    const tx = await program.methods.initialize().rpc();
+    const tx = await program.methods.initialize().accounts({
+      baseAccount: baseAccount.publicKey,
+      user: provider.wallet.publicKey,
+      systemProgram: anchor.web3.SystemProgram.programId,
+    }).signers([baseAccount]).rpc();
+
     console.log("Your transaction signature", tx);
   });
 
